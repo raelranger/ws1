89ca43b2b695e8326f359e07886c184832792f5b
real.mac <engraving09@gmail.com>
2021-08-10 18:31:01 +0700

    add footer

--- contact.php
+++ contact.php
@@ -97,51 +97,7 @@
     </section>
     
     <!-- Section Footer -->
-    <footer class="semi-footer p-5 text-center text-md-left">
-        <div class="row">
-            <div class="section-footer col-md-4">
-                <a class="navbar-brand" href="index.php">
-                    <img src="assets/Foxi.png" width="35" height="30" class="d-inline-block align-top" alt="">
-                    <span class="text-danger"> Foxi</span> 
-                </a>
-
-            </div>
-            <div class="section-footer  col-md-4">
-                NEED HELP ?
-                <ul class="grop-drawer">
-                    <li class="footer-item">
-                        <a class="footer-link" href="">Contact</a>
-                    </li>
-                    <li class="footer-item">
-                        <a class="footer-link" href="">Shopping Mall</a>
-                    </li>
-                    <li class="footer-item">
-                        <a class="footer-link" href="">Service</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="section-footer col-md-4">
-                FIND US ON
-                <ul class="grop-drawer">
-                    <li class="footer-item">
-                        <a target="_blank" class="footer-link fab fa-facebook-f"  href=""> Facebook</a>
-                    </li>
-                    <li class="footer-item">
-                        <a target="_blank" class="footer-link fab fa-twitter" href=""> Twitter</a>
-                    </li>
-                    <li class="footer-item">
-                        <a target="_blank" class="footer-link fab fa-instagram" href=""> Instagram</a>
-                    </li>
-                    <li class="footer-item">
-                        <a target="_blank" class="footer-link fab fa-youtube" href=""> Youtube</a>
-                    </li>
-                    <li class="footer-item">
-                        <a target="_blank" class="footer-link fab fa-line" href=""> Line</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+    <?php include_once('includes/footer.php')?>
 
 
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
